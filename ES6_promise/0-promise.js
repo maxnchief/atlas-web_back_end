ea6946ac02a219545d54f9f422f6373b787283b0
maxnchief <logandogan@gmail.com>
2025-03-12 15:48:55 -0500

    refactor getResponseFromAPI to handle success and failure cases

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,7 +1,11 @@
 function getResponseFromAPI() {
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('Response from API');
-        }, 1000);
+        const success = true;
+
+        if (success) {
+            resolve("API call succeeded");
+        } else {
+            reject("API call failed");
+        }
     });
 }
