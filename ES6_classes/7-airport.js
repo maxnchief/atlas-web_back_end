260ad777efe6bb19c7610f47e3400eae98343c03
maxnchief <logandogan@gmail.com>
2025-03-12 22:05:11 -0500

    refactor: simplify Airport class and update toString method format

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,12 +1,8 @@
-class Airport {
-    constructor(name, code) {
-        this._name = name;
-        this._code = code;
-    }
-
-    toString() {
-        return this._code;
-    }
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+  
+  toString() { return `[object ${this._code}]`; }
 }
-
-export default Airport;
