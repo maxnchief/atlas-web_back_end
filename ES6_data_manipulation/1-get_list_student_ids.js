c002bce458d29c7cbc5d539ae0c8535a43c2a1cf
maxnchief <logandogan@gmail.com>
2025-03-12 22:24:58 -0500

    an absolute linting

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,7 @@
-function getListStudentIds(students) {
-    if (!Array.isArray(students)) {
-        return [];
-    }
-    return students.map(student => student.id);
+export default function getListStudentIds(listOfObjects) {
+  let listOfIds = [];
+  if (Array.isArray(listOfObjects)) {
+    listOfIds = listOfObjects.map((object) => object.id);
+  }
+  return listOfIds;
 }
