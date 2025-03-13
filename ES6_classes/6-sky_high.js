532eb1fc5720a24081dccb5bfdd28a35d6996589
maxnchief <logandogan@gmail.com>
2025-03-12 22:07:09 -0500

    refactor: streamline SkyHighBuilding class and improve evacuation warning message

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,18 +1,16 @@
-import Building from './5-building.js';
+import Building from './5-building';
 
-class SkyHighBuilding extends Building {
-    constructor(sqft, floors) {
-        super(sqft);
-        this._floors = floors;
-    }
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-    get floors() {
-        return this._floors;
-    }
+  get floors() {
+    return this._floors;
+  }
 
-    evacuationWarningMessage() {
-        return `Evacuate slowly the ${this._floors} floors.`;
-    }
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
-
-export default SkyHighBuilding;
