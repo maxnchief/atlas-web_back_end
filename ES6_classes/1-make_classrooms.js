f879165bb3768ffce93cd61d8d4979998f3a11b5
maxnchief <logandogan@gmail.com>
2025-03-12 21:49:16 -0500

    lint

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 function initializeRooms() {
   return [
     new ClassRoom(19),
     new ClassRoom(20),
-    new ClassRoom(34)
+    new ClassRoom(34),
   ];
 }
 
