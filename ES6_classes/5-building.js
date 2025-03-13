364100ea9a9f7480a4798a593ef6a0d693e205d6
maxnchief <logandogan@gmail.com>
2025-03-12 22:05:54 -0500

    refactor: update Building class to enforce subclassing and improve error handling

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,18 +1,16 @@
-class Building {
+export default class Building {
     constructor(sqft) {
-        if (new.target === Building) {
-            throw new TypeError('Cannot construct Building instances directly');
-        }
-        this._sqft = sqft;
+      if (new.target !== Building) {
+        this.evacuationWarningMessage();
+      }
+      this._sqft = sqft;
     }
-
+  
     get sqft() {
-        return this._sqft;
+      return this._sqft;
     }
-
+  
     evacuationWarningMessage() {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
+      throw new Error('Class extending Building must override evacuationWarningMessage', this);
     }
-}
-
-export default Building;
+  }
