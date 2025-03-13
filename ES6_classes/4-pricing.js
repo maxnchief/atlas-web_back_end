537a1d85643d3bd8a1800ea1d0747beb342491a6
maxnchief <logandogan@gmail.com>
2025-03-12 22:00:12 -0500

    linted

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,38 +1,38 @@
 import Currency from './3-currency.js';
 
 class Pricing {
-    constructor(amount, currency) {
-        this._amount = amount;
-        this._currency = currency;
-    }
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-    // Getter and Setter for amount
-    get amount() {
-        return this._amount;
-    }
+  // Getter and Setter for amount
+  get amount() {
+    return this._amount;
+  }
 
-    set amount(value) {
-        this._amount = value;
-    }
+  set amount(value) {
+    this._amount = value;
+  }
 
-    // Getter and Setter for currency
-    get currency() {
-        return this._currency;
-    }
+  // Getter and Setter for currency
+  get currency() {
+    return this._currency;
+  }
 
-    set currency(value) {
-        this._currency = value;
-    }
+  set currency(value) {
+    this._currency = value;
+  }
 
-    // Method to display full price
-    displayFullPrice() {
-        return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-    }
+  // Method to display full price
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
 
-    // Static method to convert price
-    static convertPrice(amount, conversionRate) {
-        return amount * conversionRate;
-    }
+  // Static method to convert price
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
 
 export default Pricing;
