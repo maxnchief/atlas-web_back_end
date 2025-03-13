53a7a1328864ac861734898e6d01f797014d7184
maxnchief <logandogan@gmail.com>
2025-03-12 21:51:26 -0500

    lint

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,54 +1,54 @@
 class HolbertonCourse {
-    constructor(name, length, students) {
-        this._name = this._validateString(name, 'Name');
-        this._length = this._validateNumber(length, 'Length');
-        this._students = this._validateArrayOfStrings(students, 'Students');
-    }
+  constructor(name, length, students) {
+    this._name = this._validateString(name, 'Name');
+    this._length = this._validateNumber(length, 'Length');
+    his._students = this._validateArrayOfStrings(students, 'Students');
+  }
 
-    _validateString(value, attributeName) {
-        if (typeof value !== 'string') {
-            throw new TypeError(`${attributeName} must be a string`);
-        }
-        return value;
+  _validateString(value, attributeName) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${attributeName} must be a string`);
     }
+      return value;
+  }
 
-    _validateNumber(value, attributeName) {
-        if (typeof value !== 'number') {
-            throw new TypeError(`${attributeName} must be a number`);
-        }
-        return value;
+  _validateNumber(value, attributeName) {
+    if (typeof value !== 'number') {
+      throw new TypeError(`${attributeName} must be a number`);
     }
+      return value;
+  }
 
-    _validateArrayOfStrings(value, attributeName) {
-        if (!Array.isArray(value) || !value.every(item => typeof item === 'string')) {
-            throw new TypeError(`${attributeName} must be an array of strings`);
-        }
-        return value;
+  _validateArrayOfStrings(value, attributeName) {
+    if (!Array.isArray(value) || !value.every(item => typeof item === 'string')) {
+      throw new TypeError(`${attributeName} must be an array of strings`);
     }
+      return value;
+  }
 
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    set name(value) {
-        this._name = this._validateString(value, 'Name');
-    }
+  set name(value) {
+    this._name = this._validateString(value, 'Name');
+  }
 
-    get length() {
-        return this._length;
-    }
+  get length() {
+    return this._length;
+  }
 
-    set length(value) {
-        this._length = this._validateNumber(value, 'Length');
-    }
+  set length(value) {
+    this._length = this._validateNumber(value, 'Length');
+  }
 
-    get students() {
-        return this._students;
-    }
+  get students() {
+    return this._students;
+  }
 
-    set students(value) {
-        this._students = this._validateArrayOfStrings(value, 'Students');
-    }
+  set students(value) {
+    this._students = this._validateArrayOfStrings(value, 'Students');
+  }
 }
 
 export default HolbertonCourse;
