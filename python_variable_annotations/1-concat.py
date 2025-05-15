11d1690053d97ed43b33c9798bafe44802a455b0
maxnchief <logandogan@gmail.com>
2025-05-15 11:00:40 -0500

    add function to concatenate two strings with documentation

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+def concat(a: str, b: str) -> str:
+    """
+    Concatenate two strings.
+
+    Args:
+        a (str): The first string.
+        b (str): The second string.
+
+    Returns:
+        str: The concatenated string.
+    """
+    return a + b
