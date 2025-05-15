20fa0c73f8f8a78b6cf06329b858dc4cf57df1c1
maxnchief <logandogan@gmail.com>
2025-05-15 11:26:07 -0500

    add floor function with documentation

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Return the largest integer less than or equal to n.
+
+    Args:
+        n (float): The number to floor.
+
+    Returns:
+        int: The floored integer.
+    """
+
+    return math.floor(n)
