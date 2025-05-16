cf40a52659a8556fdca99eb9ba0e3db30638beed
maxnchief <logandogan@gmail.com>
2025-05-16 15:19:05 -0500

    second blank line

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,6 +3,7 @@
 
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple with the string and the square of the number."""
     return (k, float(v**2))
