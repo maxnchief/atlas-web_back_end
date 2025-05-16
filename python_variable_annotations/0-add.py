f807fa2f0b4ea5ac05fb46351ee970385ee2cb9f
maxnchief <logandogan@gmail.com>
2025-05-16 12:54:52 -0500

    just trying to document for intranet checker

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 def add(a: float, b: float) -> float:
+    """
+    Module: 0-add
+
+    This module provides a function to add two floating-point numbers.
+
+    Functions:
+        add(a: float, b: float) -> float:
+            Returns the sum of two floating-point numbers.
+
+    Example:
+        >>> add(2.5, 3.1)
+        5.6
+    """
     """Add two numbers together."""
     return a + b
