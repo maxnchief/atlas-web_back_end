2ffb5eb8ad9ea60c7ee0fd95985bed0272dcfce7
maxnchief <logandogan@gmail.com>
2025-05-16 15:07:06 -0500

    another blank line

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -3,6 +3,7 @@
 
 from typing import List, Union
 
+
 def sum_mixed_list(mxd_Lst: List[Union[int, float]]) -> float:
     """Returns the sum of a mixed list of integers and floats."""
     return sum(mxd_Lst)
