5674cba8c6194dc3fac4ea41550b56d37b790909
maxnchief <logandogan@gmail.com>
2025-05-17 13:02:41 -0500

    reduced to 79 characters?

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawn wait_random n times with max_delay and return list of delays in ascending order."""
+    """Spawn wait_random n times with max_delay, returns delays in order"""
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     delays = []
     for task in asyncio.as_completed(tasks):
