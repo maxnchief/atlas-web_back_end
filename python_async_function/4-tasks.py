4b266e2060c627e89c60132768f99e3cb4d29f5a
maxnchief <logandogan@gmail.com>
2025-05-17 14:06:41 -0500

    added extra newline

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -5,6 +5,7 @@ import asyncio
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
