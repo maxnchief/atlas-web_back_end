f669276205d4c329351a8396fce64a49788abdb4
maxnchief <logandogan@gmail.com>
2025-05-18 21:29:46 -0500

    fixed the Pycodestyle

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,7 +7,8 @@ Yields:
 
 Usage:
     This function is an asynchronous generator that yields a new random float
-    every second, for a total of 10 values. It can be used in an async for loop.
+    every second, for a total of 10 values. 
+    It can be used in an async for loop.
 
 Example:
     async for number in async_generator():
@@ -21,7 +22,8 @@ from typing import AsyncGenerator, List
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Yield a random number between 0 and 10, 10 times, waiting 1 second each time."""
+    """Yield a random number between 0 and 10, 10 times,
+    waiting second each time."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
@@ -31,5 +33,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """Collects 10 random numbers from async_generator and returns them as a list."""
+    """Collects 10 random numbers from async_generator. 
+    Returns them as a list."""
     return [num async for num in async_generator()]
