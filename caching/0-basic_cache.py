0d07c8e7da26e83112290c2eaef39d8379c7ce81
maxnchief <logandogan@gmail.com>
2025-05-22 11:40:05 -0500

    fix: improve condition checks in put and get methods for BasicCache

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,15 +1,16 @@
 BaseCaching = __import__('base_caching').BaseCaching
 
 class BasicCache(BaseCaching):
+    """Basic caching system with no limit."""
 
     def put(self, key, item):
         """Assign to the dictionary self.cache_data the item value for the key key."""
-        if key and item:
+        if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
         """Return the value in self.cache_data linked to key."""
-        if key and key in self.cache_data:
+        if key is None or key not in self.cache_data:
             return None  
         return self.cache_data[key]
         
