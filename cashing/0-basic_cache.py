33fe2a85a9545a45df6119c45113bd797c40cf4b
maxnchief <logandogan@gmail.com>
2025-05-22 11:33:46 -0500

    feat: implement BasicCache class with put and get methods

---
+++ cashing/0-basic_cache.py
@@ -0,0 +1,15 @@
+BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCache(BaseCaching):
+
+    def put(self, key, item):
+        """Assign to the dictionary self.cache_data the item value for the key key."""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Return the value in self.cache_data linked to key."""
+        if key and key in self.cache_data:
+            return None  
+        return self.cache_data[key]
+        
---
+++ cashing/README.md
@@ -0,0 +1,2 @@
+This is the directory for the cashing project on Atlas Intranet
+
