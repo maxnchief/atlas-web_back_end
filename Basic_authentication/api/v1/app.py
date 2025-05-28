6516a04642a379b399a497cc79cbe106edd9afd2
maxnchief <logandogan@gmail.com>
2025-05-28 17:23:14 -0500

    pycode style fixes

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -44,7 +44,11 @@ def before_request_handler():
     """Handler executed before each request to check authentication."""
     if auth is None:
         return
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None:
