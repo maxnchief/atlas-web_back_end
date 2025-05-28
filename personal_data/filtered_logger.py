02eff59779fd1bb3de806c9682bc611848a815c0
maxnchief <logandogan@gmail.com>
2025-05-27 19:44:55 -0500

    pycode style validationi

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,6 +3,9 @@
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """Obfuscate specified fields in a log message."""
-    return re.sub(rf'({"|".join(fields)})=([^{separator}]*)', rf'\1={redaction}', message)
+    pattern = rf'({"|".join(fields)})=([^{separator}]*)'
+    return re.sub(pattern, rf'\1={redaction}', message)
