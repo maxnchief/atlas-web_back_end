538b72dee4d03c0a7a41337461e96aa66c814abe
maxnchief <logandogan@gmail.com>
2025-05-27 20:05:36 -0500

    Add get_db function to establish a database connection using environment variables

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,6 +3,9 @@
 import re
 from typing import List
 import logging
+import os
+import mysql.connector
+from mysql.connector.connection import MySQLConnection
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
@@ -42,3 +45,17 @@ def get_logger() -> logging.Logger:
     stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
     logger.handlers = [stream_handler]
     return logger
+
+
+def get_db() -> MySQLConnection:
+    """Returns a connector to the secure Holberton database using environment variables."""
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+    return mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=db_name
+    )
