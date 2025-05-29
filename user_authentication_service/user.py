3ee4af8389cd941f811f613cc08490e61c4a1d16
maxnchief <logandogan@gmail.com>
2025-05-28 20:19:25 -0500

    Refactor: Update docstring in User class and adjust formatting in user.py

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
-from sqlalchemy import Column, Integer, String
+""" SQLAlchemy model for User
+"""
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
-class User(Base):
-    """SQLAlchemy model for the users table."""
-    __tablename__ = 'users'
 
+class User(Base):
+    """ User class """
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
