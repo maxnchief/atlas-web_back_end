89681e3d47e2dd9ea3c3e6e52bf608bc1e685d08
maxnchief <logandogan@gmail.com>
2025-06-14 18:43:56 -0500

    Add missing comment header to users table creation script

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,4 @@
+-- This SQL script creates a table named 'users' with the following columns:
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
