f59236b07ac878f92d372a1166f216e308a8c193
maxnchief <logandogan@gmail.com>
2025-06-14 18:48:13 -0500

    Fix id column definition in users table creation script

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- This SQL script creates a table named 'users' with the following columns:
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    id INTEGER NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(225)
+    name VARCHAR(255)
 );
