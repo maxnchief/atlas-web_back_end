6d4de30df5631ebb00636bb440e89344ad625ab9
maxnchief <logandogan@gmail.com>
2025-06-21 15:28:59 -0500

    refactor: convert Utils object to standalone calculateNumber function

--- unittests_in_js/utils.js
+++ unittests_in_js/utils.js
@@ -1,17 +1,17 @@
-const Utils = {
-    calculateNumber: function(type, a, b) {
-        const num1 = Math.round(a);
-        const num2 = Math.round(b);
+function calculateNumber(type, a, b) {
+    const num1 = Math.round(a);
+    const num2 = Math.round(b);
 
-        if (type === 'SUM') {
-            return num1 + num2;
-        } else if (type === 'SUBTRACT') {
-            return num1 - num2;
-        } else if (type === 'DIVIDE') {
-            if (num2 === 0) {
-                return 'Error';
-            }
-            return num1 / num2;
+    if (type === 'SUM') {
+        return num1 + num2;
+    } else if (type === 'SUBTRACT') {
+        return num1 - num2;
+    } else if (type === 'DIVIDE') {
+        if (num2 === 0) {
+            return 'Error';
         }
+        return num1 / num2;
     }
-};
+}
+
+module.exports = { calculateNumber };
