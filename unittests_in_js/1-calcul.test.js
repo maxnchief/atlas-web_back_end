08a165ac39676a69237c2122cd5b0f533649c7ed
maxnchief <logandogan@gmail.com>
2025-06-21 14:44:51 -0500

    refactor: update test descriptions to include type for SUM, SUBTRACT, and DIVIDE

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', function () {
-  describe('SUM', function () {
+  describe('type=SUM', function () {
     it('should return the sum of rounded numbers', function () {
       assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
       assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
@@ -12,24 +12,26 @@ describe('calculateNumber', function () {
     });
   });
 
-  describe('SUBTRACT', function () {
+  describe('type=SUBTRACT', function () {
     it('should return the difference of rounded numbers', function () {
       assert.strictEqual(calculateNumber('SUBTRACT', 5, 3), 2);
       assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
       assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 3.7), -2);
       assert.strictEqual(calculateNumber('SUBTRACT', -1.2, -3.7), 3);
-      assert.strictEqual(calculateNumber('SUBTRACT', -1.5, -3.7), 2);
+      assert.strictEqual(calculateNumber('SUBTRACT', -1.5, -3.7), 3); // fixed expected value
     });
   });
 
-  describe('DIVIDE', function () {
+  describe('type=DIVIDE', function () {
     it('should return the division of rounded numbers', function () {
       assert.strictEqual(calculateNumber('DIVIDE', 8, 2), 4);
       assert.strictEqual(calculateNumber('DIVIDE', 7.5, 2.1), 4);
       assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
       assert.strictEqual(calculateNumber('DIVIDE', -4.5, 2), -2);
     });
+  });
 
+  describe('type=DIVIDE and return error', function () {
     it('should return "Error" when dividing by zero', function () {
       assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0.2), 'Error');
       assert.strictEqual(calculateNumber('DIVIDE', 1.4, -0.2), 'Error');
