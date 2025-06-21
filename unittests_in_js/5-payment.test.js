34ddc1414f968b67314998eb922ca09f338daf2a
maxnchief <logandogan@gmail.com>
2025-06-21 15:44:35 -0500

    feat: add sendPaymentRequestToApi function and corresponding tests

---
+++ unittests_in_js/5-payment.test.js
@@ -0,0 +1,26 @@
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payments');
+
+describe('sendPaymentRequestToApi', function () {
+  let consoleSpy;
+
+  beforeEach(function () {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(function () {
+    consoleSpy.restore();
+  });
+
+  it('should log "The total is: 120" and be called once for (100, 20)', function () {
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledOnce(consoleSpy);
+    sinon.assert.calledWithExactly(consoleSpy, 'The total is: 120');
+  });
+
+  it('should log "The total is: 20" and be called once for (10, 10)', function () {
+    sendPaymentRequestToApi(10, 10);
+    sinon.assert.calledOnce(consoleSpy);
+    sinon.assert.calledWithExactly(consoleSpy, 'The total is: 20');
+  });
+});
