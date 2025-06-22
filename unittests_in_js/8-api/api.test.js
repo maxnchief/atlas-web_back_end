971a3abe51be44abff40ff665a2736b4a58c458b
maxnchief <logandogan@gmail.com>
2025-06-22 13:28:37 -0500

    refactor: improve test descriptions for index page endpoints

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -2,21 +2,15 @@ const request = require('supertest');
 const app = require('./api');
 
 describe('Index page', () => {
-  it('should return status code 200', (done) => {
+  it('GET / should return status code 200', (done) => {
     request(app)
       .get('/')
       .expect(200, done);
   });
 
-  it('should return the correct message', (done) => {
+  it('GET / should return "Welcome to the payment system"', (done) => {
     request(app)
       .get('/')
       .expect('Welcome to the payment system', done);
-  });
-
-  it('should return text content-type', (done) => {
-    request(app)
-      .get('/')
-      .expect('Content-Type', /text\/html/, done);
   });
 });
