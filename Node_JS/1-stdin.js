00231138a4596dd7db017213cc2b50a0a92fc645
maxnchief <logandogan@gmail.com>
2025-06-25 13:10:02 -0500

    fix: correct welcome message and ensure proper handling of input data

--- Node_JS/1-stdin.js
+++ Node_JS/1-stdin.js
@@ -1,5 +1,10 @@
-process.stdout.write('Welcome to Holberton School! What is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-    const name = data.toString()
-})
+    const name = data.toString().trim();
+    process.stdout.write(`Your name is: ${name}\n`);
+});
+
+process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+});
