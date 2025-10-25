847a7b91b6094df160a3133707e0a1e305223c32
maxnchief <logandogan@gmail.com>
2025-10-25 11:39:52 -0500

    chore: improve module documentation for clarity

--- Node_JS/4-http.js
+++ Node_JS/4-http.js
@@ -1,3 +1,8 @@
+/**
+ * This module uses Node's built-in 'http' to create a server which sets a 200 status code
+ * and the 'Content-Type' header to 'text/plain' for every incoming request, then ends the
+ * response with 'Hello Holberton School!'.
+ */
 const http = require('http');
 
 const app = http.createServer((req, res) => {
