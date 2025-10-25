08d0d7b9f58e11be360b92134646e3722813235c
maxnchief <logandogan@gmail.com>
2025-10-25 12:03:47 -0500

    refactor: update API tests to use request library and improve test cases

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,16 +1,30 @@
-const request = require('supertest');
-const app = require('./api');
+const request = require('request');
+const { expect } = require('chai');
 
 describe('Index page', () => {
-  it('GET / should return status code 200', (done) => {
-    request(app)
-      .get('/')
-      .expect(200, done);
+  const url = 'http://localhost:7865/';
+
+  it('Correct status code?', (done) => {
+    request.get(url, (err, res, _body) => {
+      expect(err).to.be.null;
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
   });
 
-  it('GET / should return "Welcome to the payment system"', (done) => {
-    request(app)
-      .get('/')
-      .expect('Welcome to the payment system', done);
+  it('Correct result?', (done) => {
+    request.get(url, (err, _res, body) => {
+      expect(err).to.be.null;
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('Other?', (done) => {
+    request.get(url, (err, res, _body) => {
+      expect(err).to.be.null;
+      expect(res.headers['content-type']).to.match(/text\/html/);
+      done();
+    });
   });
 });
