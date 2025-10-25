78decfe984e6e0bf290174f5e588ab4aa69c6e78
maxnchief <logandogan@gmail.com>
2025-10-25 13:26:06 -0500

    feat: implement job creator for push notification using Kue

---
+++ queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,24 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const jobData = {
+  phoneNumber: '+15555550123',
+  message: 'This is a test notification',
+};
+
+const job = queue
+  .create('push_notification_code', jobData)
+  .save((err) => {
+    if (!err) {
+      console.log(`Notification job created: ${job.id}`);
+    }
+  });
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
